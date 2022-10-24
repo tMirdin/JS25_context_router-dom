0020d6f64b5b8e2a050207eccaa52dccfa48ffef
tmirdin <mirdin.981@gmail.com>
2022-10-24 10:20:54 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AddTodo from "./Components/AddTodo/AddTodo";
+
+const App = () => {
+  return (
+    <div>
+      <AddTodo />
+    </div>
+  );
+};
+
+export default App;
